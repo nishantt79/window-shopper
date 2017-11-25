a4287a9bb6a9f4a72be01eef91dfe50f01949cb6
Nishant Taneja <techlabs@Techs-iMac.local>
2017-11-26 00:39:45 +0530

    2nd lesson

--- window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -8,21 +8,37 @@
 
 import UIKit
 
+@IBDesignable
+
 class CurrencyTxtField: UITextField {
+    
+    
+    override func prepareForInterfaceBuilder() {
+        customView()
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        customView()
+        
+        }
+    
+    func customView() {
+        
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
         
         
         if let p = placeholder {
-             let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)] )
+            let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)] )
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    
         }
-        
-        
+    
+    
     }
+        
+    
 }
